51b2de50362a735fff3181383747fafb27da3b45
apurvaPrateek <prateekapurva118@gmail.com>
2024-03-09 21:12:32 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let selectField = document.getElementById("select-field");
+let options = document.getElementById("select-options");
+let arrow = document.querySelector("#select-field img");
+let Items = document.querySelectorAll("li");
+let input = document.querySelector("input");
+
+function fieldSelect(){
+    if(options.style.display == "none") {options.style.display = "block";
+    arrow.style.transform = "rotate(180deg)";}
+    else {options.style.display = "none";
+    arrow.style.transform = "rotate(0)";}
+}
+
+selectField.addEventListener("click", fieldSelect);
+
+Items.forEach((item)=>{
+    item.addEventListener("click",()=>{
+        fieldSelect();
+        if(item.innerText == "none")
+        selectField.querySelector("p") = "Select Social Media";
+        selectField.querySelector("p").innerHTML = item.innerText;
+
+        input.value = item.innerText;
+
+        console.log(input.value);
+    })
+})
